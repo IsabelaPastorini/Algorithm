57b752e8321c5924d7ca52ce7f37f118cb98a98a
Isabela Pastorini <isabela.pastorini643@gmail.com>
2020-01-02 23:43:12 -0300

    Update on .vs code, new problem

---
+++ division_2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <string>
+
+//using namespace std;
+//Given a string of binary, calculate how many 
+//numbers are divided by 2.
+int total_divisions(){
+    return 0;
+}
+
+
+int main(){
+   std::string binary = "1001";
+    for(int i = 0; i < binary.size(); i++){
+        printf("%c \n", binary[i]);
+    }
+    return 0;
+}
